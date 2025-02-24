3a8d9953da9b937ecc9ad8ed0cb76909cff72c14
jwyche915 <191650353+jwyche915@users.noreply.github.com>
2025-02-24 10:09:47 -0700

    completed 7SD entity description

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -39,6 +39,22 @@ end sevenseg_decoder;
 architecture Behavioral of sevenseg_decoder is
 
 begin
-
+    with i_Hex select
+        o_seg_n <=  x"40" when x"0",
+                    x"79" when x"1",
+                    x"24" when x"2",
+                    x"30" when x"3",
+                    x"19" when x"4",
+                    x"12" when x"5",
+                    x"02" when x"6",
+                    x"78" when x"7",
+                    x"00" when x"8",
+                    x"18" when x"9",
+                    x"08" when x"A",
+                    x"03" when x"B",
+                    x"27" when x"C",
+                    x"21" when x"D",
+                    x"06" when x"E",
+                    x"0E" when x"F";
 
 end Behavioral;
